66a1a261afbf5f0d89ca88caefc67cce7969b81d
emrebiyik <emrebiyik34@gmail.com>
2025-08-22 17:40:37 +0200

    updated

--- start.sh
+++ start.sh
@@ -1,1 +1,8 @@
-python init_db.py && uvicorn main:app --host 0.0.0.0 --port 8000
+#!/bin/bash
+set -e
+
+echo "Starting initialization..."
+python init_db.py
+
+echo "Starting server..."
+uvicorn main:app --host 0.0.0.0 --port $PORT
